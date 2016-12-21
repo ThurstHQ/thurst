622511b634f5d7a8a0a790360bea53e59ae7ba26
Vasyl Onopriienko <vo@codemotion.eu>
2016-12-21 13:37:57 +0200

    api for authorization

--- server.js
+++ server.js
@@ -31,17 +31,17 @@ require('./config/passport')(passport);
 var apiRoutes = express.Router();
 
 apiRoutes.post('/signup', function(req, res) {
-    if (!req.body.name || !req.body.password) {
-        res.json({success: false, msg: 'Please pass name and password.'});
+    if (!req.body.email || !req.body.password) {
+        res.json({success: false, msg: 'Please pass email and password.'});
     } else {
         var newUser = new User({
-            name: req.body.name,
+            email: req.body.email,
             password: req.body.password
         });
         // save the user
         newUser.save(function(err) {
             if (err) {
-                return res.json({success: false, msg: 'Username already exists.'});
+                return res.json({success: false, msg: 'Email already exists.'});
             }
             res.json({success: true, msg: 'Successful created new user.'});
         });
@@ -50,7 +50,7 @@ apiRoutes.post('/signup', function(req, res) {
 
 apiRoutes.post('/authenticate', function(req, res) {
     User.findOne({
-        name: req.body.name
+        email: req.body.email
     }, function(err, user) {
         if (err) throw err;
 
@@ -77,14 +77,14 @@ apiRoutes.get('/memberinfo', passport.authenticate('jwt', { session: false}), fu
     if (token) {
         var decoded = jwt.decode(token, config.secret);
         User.findOne({
-            name: decoded.name
+            email: decoded.email
         }, function(err, user) {
             if (err) throw err;
 
             if (!user) {
                 return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
             } else {
-                res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
+                res.json({success: true, msg: 'Welcome in the member area ' + user.email + '!'});
             }
         });
     } else {
@@ -105,9 +105,7 @@ getToken = function (headers) {
     }
 };
 
-// connect the api routes under /api/*
 app.use('/api', apiRoutes);
 
-// Start the server
 app.listen(port);
-console.log('There will be server: http://localhost:' + port);
+console.log('Server working...');
