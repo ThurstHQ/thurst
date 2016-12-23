c6bf9fdf4f0d7217ce9cdec2585f9db6ba9f1381
Vasyl Onopriienko <vo@codemotion.eu>
2016-12-23 16:15:51 +0200

    allow options

--- server.js
+++ server.js
@@ -10,9 +10,15 @@ var express     = require('express'),
 require('./config/passport')(passport);
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    if ( req.method === 'OPTIONS' ) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        console.log('OPTIONS SUCCESS');
+        res.end();
+    }
+    else {
+        next();
+    }
 });
 
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
