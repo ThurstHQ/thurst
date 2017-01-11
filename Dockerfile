9e439762c102e0dd75e6e7c11387dde830df142c
Vasyl Onopriienko <vo@codemotion.eu>
2017-01-11 16:01:11 +0200

    test upload

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,9 @@ WORKDIR /app
 COPY . .
 RUN npm install --production
 
-#RUN mkdir /app/public
-#VOLUME test:/app/public
+RUN mkdir ~/.aws
+RUN touch ~/.aws/credentials
+RUN echo "[default]\naws_access_key_id = ${AKI}\naws_secret_access_key = ${SAK}" > ~/.aws/credentials
 
 CMD yum install imagemagick
 # if we don't use this specific form, SIGINT/SIGTERM doesn't get forwarded
--- app/global/uploadfile.js
+++ app/global/uploadfile.js
@@ -18,44 +18,73 @@ exports.uploadFiles = function (req, res, next) {
     var pathForSave = path.join('public', 'images', userId.toString());
 
     if (req.body.avatar) {
-        var base64Data = req.body.avatar.replace(/^data:image\/jpeg;base64,/, "");
-        console.log();
+        var AWS = require('aws-sdk');
 
-        mkdirp(pathForSave, function (err) {
-            if (err) console.log(err);
-            var randomString = randomstring.generate({ length: 4 }),
-                pathToImg = path.join(pathForSave, userId.toString() + '-' + randomString + ".jpeg");
-            console.log(pathForSave);
-            console.log(pathToImg);
-            fs.writeFile(pathToImg, base64Data, 'base64', function(err, data) {
-                if (err) {
-                    return res.status(500).json({message: "Something wrong... You can\'t upload file"})
-                }
+        // Create an S3 client
+        var s3 = new AWS.S3();
 
-                User.findByIdAndUpdate(new ObjectId(userId), {avatar: pathToImg}, function (err, user) {
-                    if (err) {
-                        return res.status(500).json({message: "Something wrong... You can\'t upload file"})
-                    }
-                    // console.log(appDir);
-                    // console.log(userFile.localPath);
-                    easyimg.resize({
-                        src: pathToImg,
-                        dst: pathToImg,
-                        width: 250, height: 250
-                    }).then(
-                        function(image) {
-                            console.log('Resized: ' + image.width + ' x ' + image.height);
-                        },
-                        function (err) {
-                            console.log(err);
-                        }
-                    );
+        // Create a bucket and upload something into it
+        var bucketName = userId.toString();
 
-                return res.json({"Success": true, "path": pathToImg})
+        var buf = new Buffer(req.body.avatar.replace(/^data:image\/\w+;base64,/, ""),'base64');
 
-                });
+        console.log('Buffer');
+        console.log(buf);
+
+        s3.createBucket({Bucket: bucketName}, function() {
+            var params = {
+                Bucket: bucketName,
+                Key: bucketName,
+                Body: buf,
+                ContentEncoding: 'base64',
+                ContentType: 'image/jpeg'
+            };
+            s3.putObject(params, function(err, data) {
+                if (err)
+                    console.log(err);
+                else
+                    console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+                res.json({"path": userId + '.jpeg' });
             });
-        })
+        });
+        // var base64Data = req.body.avatar.replace(/^data:image\/jpeg;base64,/, "");
+        // console.log();
+        //
+        // mkdirp(pathForSave, function (err) {
+        //     if (err) console.log(err);
+        //     var randomString = randomstring.generate({ length: 4 }),
+        //         pathToImg = path.join(pathForSave, userId.toString() + '-' + randomString + ".jpeg");
+        //     console.log(pathForSave);
+        //     console.log(pathToImg);
+        //     fs.writeFile(pathToImg, base64Data, 'base64', function(err, data) {
+        //         if (err) {
+        //             return res.status(500).json({message: "Something wrong... You can\'t upload file"})
+        //         }
+        //
+        //         User.findByIdAndUpdate(new ObjectId(userId), {avatar: pathToImg}, function (err, user) {
+        //             if (err) {
+        //                 return res.status(500).json({message: "Something wrong... You can\'t upload file"})
+        //             }
+        //             // console.log(appDir);
+        //             // console.log(userFile.localPath);
+        //             easyimg.resize({
+        //                 src: pathToImg,
+        //                 dst: pathToImg,
+        //                 width: 250, height: 250
+        //             }).then(
+        //                 function(image) {
+        //                     console.log('Resized: ' + image.width + ' x ' + image.height);
+        //                 },
+        //                 function (err) {
+        //                     console.log(err);
+        //                 }
+        //             );
+        //
+        //         return res.json({"Success": true, "path": pathToImg})
+        //
+        //         });
+        //     });
+        // })
     }
 
 };
