3bf01a951091e6c1d0a08cf293098ba9b4f8a2af
veremienko <anvere@pascalium.com>
2017-01-15 22:08:20 +0200

    fix refrash message

--- src/components/messages/messages.controller.js
+++ src/components/messages/messages.controller.js
@@ -15,7 +15,7 @@
         }
 
         function doRefresh() {
-            $rootScope.$emit('getUserDetail');
+            $rootScope.$emit('getProfileDetail');
             $rootScope.$broadcast('scroll.refreshComplete');
         }
 
