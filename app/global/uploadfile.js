8fdcc515ab86d1395495d71b7d8bfcbd99732e46
Vasyl Onopriienko <vo@codemotion.eu>
2017-02-07 20:13:54 +0200

    test

--- app/global/uploadfile.js
+++ app/global/uploadfile.js
@@ -22,14 +22,14 @@ exports.uploadFiles = function (req, res, next) {
         AWS.config.secretAccessKey = process.env.SAK;
         AWS.config.region = "us-west-2";
 
-        var s3Bucket = new AWS.S3( { params: {Bucket: 'images3763246283746/' + userIdString} } );
+        var s3Bucket = new AWS.S3( { params: {Bucket: 'images3763246283746'} } );
 
         // buf = new Buffer(req.body.avatar.replace(/^data:image\/\w+;base64,/, ""),'base64');
 
         var base64Data = req.body.avatar.replace(/^data:image\/\w+;base64,/, "");
 
         fs.writeFile(path.join('upload', userIdString + ".jpeg"), base64Data, 'base64', function(err) {
-            if (err) { console.log(err); }
+            if (err) { console.log('1= ', err); }
 
             easyimg.resize({
                 src: path.join('upload', userIdString + ".jpeg"),
@@ -37,28 +37,28 @@ exports.uploadFiles = function (req, res, next) {
                 width: 250, height: 250
             }).then(
                 function(image) {
-
-                    s3Bucket.createBucket({Bucket: 'images3763246283746/' + userIdString}, function(err, result) {
-                        if (err) console.log(err.stack);
+                    console.log('image = ', image);
+                    s3Bucket.createBucket({Bucket: 'images3763246283746'}, function(err, result) {
+                        if (err) console.log('2= ', err);
 
                         var data = {
                             Key: userIdString + '.jpeg',
-                            Bucket: 'images3763246283746/' + userIdString,
+                            Bucket: 'images3763246283746',
                             Body: fs.readFileSync(path.join('upload', userIdString + ".jpeg")),
                             ACL: 'public-read'
                         };
 
                         s3Bucket.putObject(data, function(err, data){
                             if (err) {
-                                if (err) console.log(err);
+                                if (err) console.log('7= ', err);
                             } else {
                                 fs.unlink(path.join('upload', userIdString + ".jpeg"), function (err, data) {
-                                    if (err) console.log(err);
+                                    if (err) console.log('6= ', err);
                                 });
 
                                 User.findByIdAndUpdate(new ObjectId(userId), {avatar: "https://s3-us-west-2.amazonaws.com/" + userIdString + "/" + userIdString + ".jpeg"}, function (err, user) {
                                     if (err) {
-                                        return res.status(500).json({message: "Something wrong... You can\'t update profile"})
+                                        console.log('5= ', err)
                                     }
                                     return res.json({"success": true, "path": "https://s3-us-west-2.amazonaws.com/" + userIdString + "/" + userIdString + ".jpeg"});
                                 });
@@ -67,9 +67,12 @@ exports.uploadFiles = function (req, res, next) {
                     });
                 },
                 function (err) {
-                    console.log(err);
+                    console.log('3= ', err);
                 }
-            );
+            )
+                .catch(function (err) {
+                    console.log('4= ', err);
+                });
         });
     }
 
